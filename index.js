c65e86dddcded37bb3ae8a1275053186f2471885
ahmedrehane <rehaneahmed49@gmail.com>
2023-11-02 20:26:10 +0100

    objet

---
+++ index.js
@@ -0,0 +1,42 @@
+alert("hello word")
+
+
+
+
+function addition(a, b) {
+	return  a + b
+}
+
+
+function circuitPower(voltage, current) {
+	return voltage * current
+}
+
+  
+function convert(minutes) {
+	return minutes * 60
+}
+
+function calcAge(age) {
+	return age * 365
+	
+}
+
+function howManySeconds(hours) {
+	return hours * 3600
+}
+
+function triArea(base, height) {
+	return (base * height) /2
+}
+
+
+function cubes(a) {
+	return a ** 3
+}
+
+function sumPolygon(n) {
+	return (n-2) * 180
+}
+
+
